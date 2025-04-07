c8128526770b3fd40e8a906e27c42392d3b18e1f
NGWS <n.gw.s@icloud.com>
2025-04-07 15:30:20 -0700

    int trunc on win screen

--- Scripts/win_screen.gd
+++ Scripts/win_screen.gd
@@ -19,14 +19,14 @@ func _process(delta: float) -> void:
 	if t >= 1.0:
 		coins_label.self_modulate.a = 1.0
 		npcs_label.self_modulate.a = 1.0
-		
+
 		var coins: int = get_node("/root/Main/Sandbox/Player").coins
 		var coins_per: float = float(coins) / float(Utils.total_coins) * 100
-		coins_label.text = "Coins Collected: " + str(coins_per) + "% (" + str(coins) + "/" + str(Utils.total_coins) + ")"
+		coins_label.text = "Coins Collected: " + str(int(round(coins_per))) + "% (" + str(coins) + "/" + str(Utils.total_coins) + ")"
 
 		var damned: int = Utils.npcs_damned
 		var damned_per: float = float(damned) / float(Utils.total_npcs) * 100
-		npcs_label.text = "NPCs Damned: " + str(damned_per) + "% (" + str(damned) + "/" + str(Utils.total_npcs) + ")"
+		npcs_label.text = "NPCs Damned: " + str(int(round(damned_per))) + "% (" + str(damned) + "/" + str(Utils.total_npcs) + ")"
 
 		if Input.is_action_just_pressed("Pause"):
 			get_tree().quit()
